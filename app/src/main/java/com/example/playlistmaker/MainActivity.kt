bc5eb30cc790117126d77943588908cc09267e93
Anna <annaparfenova514@gmail.com>
2023-10-03 14:40:15 +0300

    New screens created: 1. Main 2. Settings 3. Music 4. Search

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageSearch = findViewById<Button>(R.id.search)
+        imageSearch.setOnClickListener{
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        val imageMusic = findViewById<Button>(R.id.music)
+        imageMusic.setOnClickListener{
+            val musicIntent = Intent(this, MusicActivity::class.java)
+            startActivity(musicIntent)
+        }
+
+        val imageSetting = findViewById<Button>(R.id.setting)
+        imageSetting.setOnClickListener{
+            val searchIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.playlistmaker
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageButton>(R.id.back)
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
