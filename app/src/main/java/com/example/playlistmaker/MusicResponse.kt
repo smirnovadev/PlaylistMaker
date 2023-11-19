911b168fe456457ae18f45cc68df36e0f6b996a1
Anna <annaparfenova514@gmail.com>
2023-11-19 15:34:56 +0300

    Настроила экран поиска и добавила выполнение  поисковых запросов с помощью API

---
+++ app/src/main/java/com/example/playlistmaker/MusicResponse.kt
@@ -0,0 +1,3 @@
+package com.example.playlistmaker
+
+class MusicResponse(val results: List<Music>)
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,30 +4,52 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.playlistmaker.databinding.ActivitySearchBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchBinding
     private var userText: String = DEFAULT_TEXT
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+    private val trackList = ArrayList<Music>()
+    private val adapter = TrackAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val clearButton: View = binding.clearButton
-        val editText: EditText = binding.searchEditText
+        val editText = binding.searchEditText
         editText.setText(userText)
 
-        val toolbar = binding.toolbar
-        toolbar.setNavigationOnClickListener {
+        binding.toolbar.setNavigationOnClickListener {
             finish()
+        }
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (editText.text?.isNotEmpty() == true) {
+                    searchForTrack(editText.text.toString())
+                    hideKeyboard(editText)
+                }
+                true
+            } else {
+                false
+            }
         }
 
         editText.addTextChangedListener(
@@ -35,7 +57,7 @@ class SearchActivity : AppCompatActivity() {
 
             },
             onTextChanged = { text, start, before, count ->
-                clearButton.visibility = clearButtonVisibility(text)
+                binding.clearButton.visibility = clearButtonVisibility(text)
             },
             afterTextChanged = { text: Editable? ->
                 if (text != null) {
@@ -43,41 +65,67 @@ class SearchActivity : AppCompatActivity() {
                 }
             }
         )
-        clearButton.setOnClickListener {
+        binding.clearButton.setOnClickListener {
             editText.setText(DEFAULT_TEXT)
             val inputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(editText.windowToken, 0)
         }
 
-        val trackList = getTracksFromAssets()
+        binding.retryButton.setOnClickListener {
+            if (editText.text?.isNotEmpty() == true) {
+                searchForTrack(editText.text.toString())
+            }
+        }
+        adapter.trackList = trackList
         val recyclerView = binding.searchRecycler
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = TrackAdapter(trackList)
+        recyclerView.layoutManager = LinearLayoutManager(this@SearchActivity)
+        recyclerView.adapter = adapter
     }
 
-    private fun getTracksFromAssets(): MutableList<Track> {
-        val arguments = assets
-            .open("tracks.txt")
-            .bufferedReader()
-            .use { it.readText() }
-            .split("\n")
-            .map { it.trim() }
-        val trackList = mutableListOf<Track>()
-        var i = 0
-        while (i + 3 < arguments.size) {
-            trackList.add(
-                Track(
-                    arguments[i],
-                    arguments[i + 1],
-                    arguments[i + 2],
-                    arguments[i + 3]
-                )
-            )
-            i += 4
+    fun hideKeyboard(view: View) {
+        val inputMethodManager = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun searchForTrack(text: String) {
+        itunesService.search(text).enqueue(object : Callback<MusicResponse> {
+            override fun onResponse(
+                call: Call<MusicResponse>,
+                response: Response<MusicResponse>
+            ) {
+                if (response.code() == 200) {
+                    val body = response.body()
+                    showContent(body?.results)
+                } else {
+                    showError()
+                }
+            }
+
+            override fun onFailure(call: Call<MusicResponse>, t: Throwable) {
+                showError()
+            }
+        })
+    }
+
+    private fun showContent(tracks: List<Music>?) {
+        trackList.clear()
+        if (tracks?.isNotEmpty() == true) {
+            binding.emptyContainer.visibility = View.GONE
+            binding.searchRecycler.visibility = View.VISIBLE
+            trackList.addAll(tracks)
+            adapter.notifyDataSetChanged()
+        } else {
+            binding.emptyContainer.visibility = View.VISIBLE
+            binding.searchRecycler.visibility = View.GONE
         }
-        return trackList
+        binding.errorContainer.visibility = View.GONE
+    }
+
+    private fun showError() {
+        binding.searchRecycler.visibility = View.GONE
+        binding.errorContainer.visibility = View.VISIBLE
+        binding.emptyContainer.visibility = View.GONE
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -101,6 +149,6 @@ class SearchActivity : AppCompatActivity() {
     companion object {
         const val USER_TEXT = "USER_TEXT"
         const val DEFAULT_TEXT = ""
-
+        const val BASE_URL = "https://itunes.apple.com"
     }
 }
