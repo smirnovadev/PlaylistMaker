52f76587af9fd53d46fae5ff363d79acc0c3d788
Anna <annaparfenova514@gmail.com>
2023-11-19 15:34:56 +0300

    Настроила экран поиска и добавила выполнение  поисковых запросов с помощью API

--- app/src/main/java/com/example/playlistmaker/Music.kt
+++ app/src/main/java/com/example/playlistmaker/Music.kt
@@ -1,8 +1,9 @@
 package com.example.playlistmaker
 
-data class Track(
+
+data class Music(
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
+    val trackTimeMillis: Int,
     val artworkUrl100: String
 )
